42d3814792acc2debca4810926784898f72bc488
simongle <simongle@gmail.com>
2014-12-29 15:02:01 -0500

    Added Location module to print user's lat/long in DOM

--- app/scripts/controllers/location.js
+++ app/scripts/controllers/location.js
@@ -7,9 +7,11 @@
  * # LocationCtrl
  * Controller of the sidamoApp
  */
+(function() {
 angular.module('sidamoApp', ['geolocation'])
   .controller('LocationCtrl', function ($scope, geolocation) {
     geolocation.getLocation().then(function(data){
       $scope.coords = {lat:data.coords.latitude, long:data.coords.longitude};
     });
   });
+})();
